ef8a37d4618b5e1577516b2c041af818d5c630d5
tom-fejer <ftomi89@gmail.com>
2021-09-22 22:28:27 +0200

    webpack alias and jsconfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const path = require("path");
+const vueSrc = "./src";
+
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/learnit/'
-    : '/'
+    : '/',
+    configureWebpack: {
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, vueSrc)
+        },
+        extensions: ['.js', '.vue', '.json']
+      }
+    }
 }
