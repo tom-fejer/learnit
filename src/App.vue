375b7a25b6838c801760dd41308edfc39d494250
tom-fejer <ftomi89@gmail.com>
2021-09-30 23:45:24 +0200

    basic tailwindcss exercise

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <div></div>
+  <div class="main-container">
+    <div
+      class="
+        text-xl
+        bg-gray-100
+        text-black
+        dark:bg-gray-600
+        dark:text-gray-100
+        p-44
+      "
+    >
+      Hello World!
+    </div>
+    <Progress v-if="userIsLoggedIn()" />
+    <Login v-else />
+    <Button class="m-1"/>
+    <Button class="m-1"/>
+    <Button class="m-1"/>
+    <Button class="m-1"/>
+  </div>
 </template>
 
 <script>
-import Progress from './views/Progress.vue'
-import Login from './views/Login.vue'
-import Profile from './views/Profile.vue'
+import Progress from "./views/Progress.vue";
+import Login from "./views/Login.vue";
+import Button from "./components/elements/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Progress,
     Login,
-    Profile
-  }
-}
+    Button,
+  },
+  methods: {
+    userIsLoggedIn() {
+      return true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +46,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+.main-container {
+  @apply flex flex-col flex-wrap content-center items-center p-3;
 }
 </style>
---
+++ src/components/elements/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="
+      transition
+      duration-200
+      ease-linear
+      bg-blue-600
+      hover:bg-red-600
+      transform-gpu
+      hover:translate-x-1
+      w-24
+      h-7
+      text-gray-50
+    "
+  >
+    GOMB
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
