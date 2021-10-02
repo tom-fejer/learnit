23989f5ab6f18ac5a2e19a1767aad0b9dcc545ae
tom-fejer <ftomi89@gmail.com>
2021-10-02 19:37:34 +0200

    gitversion configured with yarn version

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+# validate git branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ $current_branch != 'master' ]; then
     echo '[WARNING] You are currently on $current_branch.'
     echo 'Please use master branch for deployment.'
     echo 'Aborting process.'
+else
+    # build
+    yarn run build
+    # get current semantic version number
+    gitversion=$(GitVersion -output json -showvariable SemVer)
+    # set new version in package.json
+    yarn version --new-version $gitversion
+    # navigate into the build output directory
+    cd dist
+    # if you are deploying to a custom domain
+    # echo 'www.example.com' > CNAME
+    git init
+    git add -A
+    git commit -m 'deploy $gitversion'
+    git push -f git@github.com:tom-fejer/learnit.git master:gh-pages
+    cd -
 fi
-# build
-yarn run build
-# navigate into the build output directory
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:tom-fejer/learnit.git master:gh-pages
-cd -
-
-# switching back to previous branch
-git checkout $current_branch
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': '/src'
-        }
-    },
-    plugins: [vue()],
-    base: process.env.NODE_ENV === 'production' ? '/learnit/' : '/'
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/learnit/' : '/'
 })
