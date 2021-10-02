0392291bda60bdc1ede70e5c442104143e01b3fc
tom-fejer <ftomi89@gmail.com>
2021-10-02 16:20:36 +0200

    changed branch validation to warning only

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -e
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ $current_branch != 'master' ]; then
     echo '[WARNING] You are currently on $current_branch.'
-    echo 'Checking out master branch.'
-    git checkout master
+    echo 'Please use master branch for deployment.'
+    echo 'Aborting process.'
 fi
 # build
 yarn run build
@@ -18,6 +18,3 @@ git add -A
 git commit -m 'deploy'
 git push -f git@github.com:tom-fejer/learnit.git master:gh-pages
 cd -
-
-# switching back to previous branch
-git checkout $current_branch
