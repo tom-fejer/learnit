87c461a6876cdd0e8f3910b4f3ef8d0fcebd1702
tom-fejer <ftomi89@gmail.com>
2021-10-02 15:47:12 +0200

    gitversion init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {alias: {'@': '/src'} },
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production'
-  ? '/learnit/'
-  : '/'
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
+    },
+    plugins: [vue()],
+    base: process.env.NODE_ENV === 'production' ? '/learnit/' : '/'
 })
