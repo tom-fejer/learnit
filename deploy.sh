b910deaf5a2e2cb3702a6cb5b0c43136d01f0e4e
tom-fejer <ftomi89@gmail.com>
2021-10-02 16:06:31 +0200

    added branch validation to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ $current_branch != 'master' ]; then
+    echo '[WARNING] You are currently on $current_branch.'
+    echo 'Checking out master branch.'
+    git checkout master
+fi
 # build
 yarn run build
 # navigate into the build output directory
@@ -12,3 +18,6 @@ git add -A
 git commit -m 'deploy'
 git push -f git@github.com:tom-fejer/learnit.git master:gh-pages
 cd -
+
+# switching back to previous branch
+git checkout $current_branch
