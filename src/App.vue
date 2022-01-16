b96ee25a145a7535c6971a3a32dc05133cc122f8
tom-fejer <ftomi89@gmail.com>
2022-01-16 15:56:53 +0100

    page transitions

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,19 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+.slide-fade-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
 </style>
